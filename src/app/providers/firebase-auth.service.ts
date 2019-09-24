7d1925e549f12b32f29e8fbdf355bbf8a6ee58b2
clemmdc <asl_34@hotmail.fr>
2019-09-24 14:21:33 +0200

    Bite

--- src/app/providers/firebase-auth.service.ts
+++ src/app/providers/firebase-auth.service.ts
@@ -3,8 +3,6 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs';
 
-ssss
-
 @Injectable({
   providedIn: 'root'
 })
